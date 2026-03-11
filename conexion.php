c4699b398cac990ea920bfccfe42162b0405f687
jorgeeelp <24.damt.jleileon@fundacionmontessori.com>
2026-03-11 14:25:02 +0100

    cambio config de conexion para bbdd en la nube (hosting)

--- conexion.php
+++ conexion.php
@@ -1,8 +1,8 @@
 <?php
-$servidor = "localhost";
-$usuario = "root";
-$password = "";
-$base_datos = "restaurante";
+$servidor = "sql112.infinityfree.com";
+$usuario = "if0_41363922";
+$password = "H27vPQvHqFKJi";
+$base_datos = "if0_41363922_restaurante";
 
 $conn = new mysqli($servidor, $usuario, $password, $base_datos);
 
